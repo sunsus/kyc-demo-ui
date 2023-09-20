0e3df557a18aa8fd1039ba853deed76b0b829f69
Patrick Dürsteler <patrick@sunsus.net>
2023-09-20 13:01:54 +0200

    Add production Profile

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // backendUrl: 'http://localhost:3000' // add this line
-  backendUrl: '/api' // add this line
+  backendUrl: 'https:&&vt-kyc.azurewebsites.net' // add this line
 };

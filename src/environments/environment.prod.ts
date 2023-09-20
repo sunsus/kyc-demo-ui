35aabdfaf65d795a3a0825c61863ee74ea495ccd
Andreas <andreas.landerer@vontobel.com>
2023-09-20 14:26:26 +0200

    Fixed environment issue

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  backendUrl: 'https://silent-relic-393813.ew.r.appspot.com' // add this line
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  backendUrl: 'http://localhost:3000'
+  production: true,
+  backendUrl: 'https://silent-relic-393813.ew.r.appspot.com',
 };
